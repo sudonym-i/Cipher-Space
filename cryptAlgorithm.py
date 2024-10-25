81a1891536205988c91bc96a1dbcc669df1ebbdb
Isaac K <isickln08@gmail.com>
2024-10-24 20:42:57 -0600

    Add files via upload

---
+++ cryptAlgorithm.py
@@ -0,0 +1,89 @@
+import math
+import random
+import string
+
+def numberRepresentation(string: str) -> list:
+    "Returns an array of numbers translated to their ASCII numerical value --> in order"
+    values = []
+
+    i = 0;
+    while i < len(string):
+        values.append(int(ord(string[i])));
+        i += 1;
+    
+    return values
+        
+
+def codeGeneration(size: int, chars=string.digits) -> str:
+    "This bad boy is courtesy of our lord and savior; Stack Overflow"
+    return ''.join(random.choice(chars) for x in range(size))
+
+
+def iterativeSeries (encryptionKey: list, keyIterations: list) -> list:
+    "Here we create a series predicated off of the attribute parameters previously assigned to each character."
+    i = 0; # <--- gross
+    encryptedMessage = []
+    while i < len(encryptionKey):
+        iTwo = 0
+        while iTwo <= keyIterations[i]:
+            encryptedMessage.append()
+        i += 1; # <--- gross
+
+
+def removeDuplicates (string: list) -> list:
+    "removes all duplicates, and produces an array including all characters utilized in this message"
+    return list(set(string))
+
+
+def attributeParameters (usedCharacters: list) -> list:
+    """**Returns as strings**  Give each letter a set of numerical parameters which will define its unique iterative series PER CHARACTER
+     --> These values will be the "key" used to encrypt, and later decrypt, the message [currently 6 parameters employed]
+    """
+    i = 0
+    keyValues = [] # <-- This array will store 6 digit integers, each index will be used for a different letter (I couldnt be bothered to figure out 2d arrays for python)
+    #                                           ** empty spaces will just become 0's **
+    while i < len(usedCharacters):
+        randomCode = (codeGeneration(6))
+        if (randomCode not in keyValues): # <-- checks and corrects for repeats 
+            keyValues.append(randomCode)
+            i += 1
+
+    return keyValues
+
+
+def main() -> None:
+
+    class messageProperties:
+        " v these values contain the entire message"
+        inNumbers = 0;
+        inPlainText = "";
+
+        " v this is inportant information but !! DOES NOT CONTAIN ENTIRE MESSAGE !! "
+        charactersInNumbers = [];
+    
+    message = messageProperties()
+    message.inPlainText = input("enter message: ")
+
+    class encryptionProperties:
+        "This keeps track of my encryption parameters for each character and iteration count (and ASCII correspondance)"
+        keys = []
+        iterationCount = []
+        correspondantASCII = [] # <-- this one is a bit more tentative
+
+    message.encryption =  encryptionProperties();
+
+    # create my message properties from 
+    message.inNumbers = numberRepresentation(message.inPlainText)
+    message.charactersInNumbers = removeDuplicates(message.inNumbers)
+    message.encryption.keys = attributeParameters(message.charactersInNumbers) # <-- Randomly generates key parameters for each character
+
+    encryptedMessage = iterativeSeries(message.encryption.keys, message.encryption.iterationCount)
+
+
+    print(f"letters as numbers:  {message.inNumbers} {type(message.inNumbers)}")
+    print(f"Characters utilized in this message:  {message.charactersInNumbers} {type(message.charactersInNumbers)}")
+    print(f"paramaters / key values:  {message.encryption.keys} {type(message.encryption.keys)}")
+
+
+
+main()
